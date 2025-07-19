ed7af89b7e8c4c1e8deafea56bc1eb077a642406
Josphat <josphat.langat@azubiafrica.org>
2025-07-19 18:34:07 +0300

    changes location of queries

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,19 +11,23 @@ output "region" {
 output "transfer_service_account" {
   description = "The service account running scheduled BigQuery queries"
   value       = var.transfer_service_account
+  sensitive   = true
 }
 
 output "alert_email_sender" {
   description = "Sender email for alerting"
   value       = var.email_sender
+  sensitive   = true
 }
 
 output "alert_email_recipient" {
   description = "Recipient email for alerting"
   value       = var.email_recipient
+  sensitive   = true
 }
 
 output "slack_webhook_url" {
   description = "Slack webhook URL used for alerts (masked)"
   value       = length(var.slack_webhook_url) > 0 ? "***********" : ""
+  sensitive   = true
 }
